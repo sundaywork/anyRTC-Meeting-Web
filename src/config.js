811dbbc872db0d4202dfc40a98cb3620c56a41be
sundaywork <sundaywork@gmail.com>
2020-05-26 17:41:08 +1200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    APP_ID: '',
-    APP_TOKEN: '',
+    APP_ID: 'arP1JA0lUwN7P53Bwm',
+    APP_TOKEN: '9fed2081cf5e67bdeeabfbfa64914fe8',
     RTC_SERVER_URL: '',//如果有部署私有云请配置私有云服务地址
 };
